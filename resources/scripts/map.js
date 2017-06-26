2542267591e50f4c4b3aabcc25119b792ea05fa8
aso2101 <andrew.ollett@gmail.com>
2017-06-26 13:12:43 +0200

    undoing problem with map.js

--- resources/scripts/map.js
+++ resources/scripts/map.js
@@ -10,14 +10,36 @@ myLayer.on('layeradd', function(e) {
 
     // http://leafletjs.com/reference.html#popup
     marker.bindPopup(popupContent);
-    
-    var name = layer.feature.properties.name;
 });
 
 myLayer.setGeoJSON(geojson);
 
 myLayer.on('mouseover', function(e) {
     e.layer.openPopup();
+});
+
+$( document ).on('ready', function(e) {
+    myLayer.eachLayer(function(layer) {
+        var name = layer.feature.properties.name;
+        var spans = document.getElementsByClassName(name);
+        if (typeof spans != 'undefined') {
+            for (i = 0; i<spans.length; i++) {
+                var span = spans.item(i);
+                var a = document.createElement("a");
+                var b = document.createTextNode(" ");
+                var linkText = document.createTextNode("(show on map)");
+                a.appendChild(linkText);
+                a.href = "javascript:void();";
+                a.onclick = function() {
+                    map.setView(layer.getLatLng());
+                    layer.openPopup();
+                    location.hash = '#';
+                };
+                span.appendChild(b);
+                span.appendChild(a);
+            }
+        }
+    });
 });
 
 function panto(e) {
@@ -34,25 +56,4 @@ function panto(e) {
             }
         }
     });
-}
-
-function addLinksToPlace() {
-    var spans = document.getElementsByClassName(placeName);
-    if (typeof spans != 'undefined') {
-        for (i = 0; i<spans.length; i++) {
-            var span = spans.item(i);
-            var a = document.createElement("a");
-            var b = document.createTextNode(" ");
-            var linkText = document.createTextNode("(show on map)");
-            a.appendChild(linkText);
-            a.href = "javascript:void();";
-            a.onclick = function() {
-                map.setView(layer.getLatLng());
-                layer.openPopup();
-                location.hash = '#';
-            };
-            span.appendChild(b);
-            span.appendChild(a);
-        }
-    }
 }
