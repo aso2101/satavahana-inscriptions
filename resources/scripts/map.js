2cb89e9e5f46dbce8e5df8126179f353b72de7c3
aso2101 <andrew.ollett@gmail.com>
2017-06-28 21:18:36 +0200

    applying fix in issue21

--- resources/scripts/map.js
+++ resources/scripts/map.js
@@ -1,44 +1,9 @@
 //Add geojson to myLayer
 var myLayer = L.mapbox.featureLayer(geojson).addTo(map);
 
-// Add popup to marker
-myLayer.eachLayer(function(marker) {
-    var feature = marker.feature;
-    
-    // Create custom popup content
-    var popupContent =  '<a href="#' + feature.properties.name + '">' + feature.properties.name + '</a>';
-            
-    // http://leafletjs.com/reference.html#popup
-    marker.bindPopup(popupContent);
-    
-});
-        
+//Trigger popup 
 myLayer.on('mouseover', function(e) {
     e.layer.openPopup();
-});
-
-$( document ).on('ready', function(e) {
-    myLayer.eachLayer(function(layer) {
-        var name = layer.feature.properties.name;
-        var spans = document.getElementsByClassName(name);
-        if (typeof spans != 'undefined') {
-            for (i = 0; i<spans.length; i++) {
-                var span = spans.item(i);
-                var a = document.createElement("a");
-                var b = document.createTextNode(" ");
-                var linkText = document.createTextNode("(show on map)");
-                a.appendChild(linkText);
-                a.href = "javascript:void();";
-                a.onclick = function() {
-                    map.setView(layer.getLatLng());
-                    layer.openPopup();
-                    location.hash = '#';
-                };
-                span.appendChild(b);
-                span.appendChild(a);
-            }
-        }
-    });
 });
 
 function panto(e) {
@@ -57,25 +22,26 @@ function panto(e) {
     });
 }
 
-map.fitBounds(myLayer.getBounds(),{maxZoom: 7});
-
-function addLinksToPlace() {
-    var spans = document.getElementsByClassName(placeName);
-    if (typeof spans != 'undefined') {
-        for (i = 0; i<spans.length; i++) {
-            var span = spans.item(i);
-            var a = document.createElement("a");
-            var b = document.createTextNode(" ");
-            var linkText = document.createTextNode("(show on map)");
-            a.appendChild(linkText);
-            a.href = "javascript:void();";
-            a.onclick = function() {
-                map.setView(layer.getLatLng());
-                layer.openPopup();
-                location.hash = '#';
-            };
-            span.appendChild(b);
-            span.appendChild(a);
-        }
-    }
-}
+//Add links to placeNames that appear on the map
+$(document).ready(function() {
+    //Center and fit all markers on map
+    map.fitBounds(myLayer.getBounds(),{maxZoom: 6});
+    //Create popups for markers and create links on page to markers
+    myLayer.eachLayer(function(marker) {
+        var feature = marker.feature;
+        var name = feature.properties.name
+        // Create custom popup content
+        var popupContent =  '<a href="#' + name + '">' + name + '</a>';  
+        // http://leafletjs.com/reference.html#popup
+        marker.bindPopup(popupContent);
+        $('#' + name).each(function(){
+            $(this).append(' <a href="' + $(this).html() + '" class="small panto">(show on map)</a> ');
+        });
+    });
+    //trigger panto() function from place titles to map markers. 
+    $( ".panto" ).click(function(e) {
+      e.preventDefault();
+      location.href = "#map";
+      panto($(this).attr('href'));
+    });
+});
